6ddb1dfd0185ae2df085ceb905a145bbeccd104e
prashmi12 <prashmi.06@gmail.com>
2021-03-19 12:59:10 +0530

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Product](  
+    [ProductId] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,  
+    [ProductName] [nvarchar](100) NOT NULL,  
+    [ProductDescription] [nvarchar] (100) NULL,  
+    [Price] [decimal] NOT NULL) 
